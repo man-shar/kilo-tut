eacd92d8f7c2f6e32765a1c51d778ffa7e142c17
manas <sharma.manas271196@gmail.com>
2018-03-26 01:34:51 +0530

    lines

--- kilo.c
+++ kilo.c
@@ -5,25 +5,38 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
+
+/* defines */
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 /* globals */
 
-struct termios orig_termios;
+struct editorConfig {
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 /* terminal */
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
   perror(s);
   exit(1);
 }
 
 void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
     die("disableRawMode");
 }
 
 void enableRawMode() {
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   atexit(disableRawMode);
 
   if (tcgetattr(STDIN_FILENO, &raw) == -1)
@@ -41,27 +54,72 @@ void enableRawMode() {
     die("tcsetattr");
 }
 
+char editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if(nread == -1 && errno != EAGAIN) die("read");
+  }
+
+  return c;
+}
+
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
+/* output */
+
+void editorDrawRows() {
+  for (int i = 0; i < E.screenrows; ++i)
+  {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  editorDrawRows();
+
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/* input */
+
+void editorProcessKey() {
+  char c = editorReadKey();
+
+  switch(c){
+    case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
+      exit(0);
+      break;
+  }
+}
+
 /* init */
 
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
+  initEditor();
   enableRawMode();
 
   while(1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
-      die("read");
-    }
-
-    if(iscntrl(c)) {
-      printf("%d\x0D\n", c);
-    }
-
-    else {
-      printf("(%d)%c\x0D\n", c, c);
-    }
-
-    if (c == 'q')
-      break;
+    editorRefreshScreen();
+    editorProcessKey();
   }
 
   return 0;

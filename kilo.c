6774c6cfe729d73b8c2c948fb046f8e665c9b9c5
manas <sharma.manas271196@gmail.com>
2018-03-27 13:56:42 +0530

    gets rows and columns. drawing tildes work.

--- kilo.c
+++ kilo.c
@@ -64,10 +64,34 @@ char editorReadKey() {
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  while(i < sizeof(buf) - 1) {
+    if(read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if(buf[i] == 'R') break;
+    i++;
+  }
+
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+  return 0;
+}
+
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
+
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
+    // if the terminal doesn't support TIOCGWINSZ
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+
+    return getCursorPosition(rows, cols);
   } else {
     *cols = ws.ws_col;
     *rows = ws.ws_row;
@@ -78,9 +102,12 @@ int getWindowSize(int *rows, int *cols) {
 /* output */
 
 void editorDrawRows() {
-  for (int i = 0; i < E.screenrows; ++i)
-  {
-    write(STDOUT_FILENO, "~\r\n", 3);
+  for (int i = 0; i < E.screenrows; ++i) {
+    write(STDOUT_FILENO, "~", 1);
+
+    if (i < E.screenrows - 1) {
+      write(STDOUT_FILENO, "\r\n", 2);
+    }
   }
 }
 
@@ -95,10 +122,10 @@ void editorRefreshScreen() {
 
 /* input */
 
-void editorProcessKey() {
+void editorProcessKeyPress() {
   char c = editorReadKey();
 
-  switch(c){
+  switch(c) {
     case CTRL_KEY('q'):
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
@@ -114,12 +141,12 @@ void initEditor() {
 }
 
 int main() {
-  initEditor();
   enableRawMode();
+  initEditor();
 
   while(1) {
     editorRefreshScreen();
-    editorProcessKey();
+    editorProcessKeyPress();
   }
 
   return 0;
